ccc0d0d1c594e5485c5380cadea74c10d97bb333
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-28 17:41:49 +0900

    Applied a GET request method

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,7 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from typing import Annotated
+from sqlalchemy.orm import Session
+from models import Todos
 import models
-from database import engine
+from database import engine, SessionLocal
 
-app = FastAPI
+app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@app.get("/")
+async def read_all(db: db_dependency):
+    return db.query(Todos).all()
