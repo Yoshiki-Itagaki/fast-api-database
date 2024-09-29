2618d5ee1a8595dfa318ece93566371f57095dd3
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-29 23:53:44 +0900

    Implemented a JSON decoding feature

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, Depends
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from pydantic import BaseModel
 from models import Users
 from passlib.context import CryptContext
@@ -7,7 +7,7 @@ from starlette import status
 from typing import Annotated
 from sqlalchemy.orm import Session
 from database import SessionLocal
-from jose import jwt
+from jose import jwt, JWTError
 from datetime import timedelta, datetime
 
 router = APIRouter()
@@ -16,6 +16,7 @@ SECRET_KEY = 'b49a4d9765afc876090649621faedfc20af80803c419b95c8d4207e205aac0a0'
 ALGORITHM = 'HS256'
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='token')
 
 
 class CreateUserRequest(BaseModel):
@@ -30,6 +31,7 @@ class CreateUserRequest(BaseModel):
 class Token(BaseModel):
     access_token: str
     token_type: str
+
 
 def get_db():
     db = SessionLocal()
@@ -56,6 +58,20 @@ def create_access_token(username: str, user_id: int, expires_delta: timedelta):
     expires = datetime.now() + expires_delta
     encode.update({'exp': expires})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could Not Validate the User')
+
+        return {'username': username, 'id': user_id}
+
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could Not Validate the User')
 
 
 @router.post("/auth/", status_code=status.HTTP_201_CREATED)
